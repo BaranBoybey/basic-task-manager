dbce9cadf43bcf4bd0630c57914902b6d7988c35
Baran Boybey <boybeybaran5@gmail.com>
2024-09-08 15:44:13 -0400

    Task class updated

--- task.py
+++ task.py
@@ -4,9 +4,18 @@ class Task:
         self.name = name_init
         self.description = description_init
         self.priority = priority
+        self.status = status
 
-    def getStatus(self):
-        return self.priority
+    def updateStatus(self):
+
+        status_update = input("Set the task status as 'completed' or 'incomplete'(C/I): ")
+
+        if status_update.lower() == "c":
+            self.status = True
+        elif status_update.lower() == "i":
+            self.status = True
+        else:
+            print("Invalid option")
 
 
 
